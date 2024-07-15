08e7eed3be9499ba7f4901aeec8f935af1c36f15
dhruv465 <dhruv.465@gmail.com>
2024-07-16 01:32:46 +0530

    chore: Update backend MongoDB connection to use separate module

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/connectDB');
 const hotelRoutes = require('./routes/hotelRoutes');
 const flightRoutes = require('./routes/flightRoutes'); // Import flightRoutes
 
@@ -11,23 +11,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
 
+// Connect to MongoDB
+connectDB();
+
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.log('Error connecting to MongoDB', err);
-});
 
 // Use routes
 app.use('/api', hotelRoutes); // Use hotelRoutes under /api
